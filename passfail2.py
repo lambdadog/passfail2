64b91512f38ced50e7ff07b06f18dd03ed78c67a
Ashlynn Anderson <pea@pea.sh>
2022-07-13 18:59:45 -0400

    Complete rewrite

--- passfail2.py
+++ passfail2.py
@@ -1,4 +1,3 @@
-# -*- mode:Python -*-
 # Pass/Fail 2
 #
 # Only offers 2 buttons to press: Pass, and Fail. Fail is equivalent
@@ -8,68 +7,70 @@
 # reviewing Hard cards, making them more difficult to acquire.
 #
 # License: GNU GPL, version 3 or later; http://www.gnu.org/copyleft/gpl.html
-# Copyright (c) 2020 Ashlynn Anderson
+# Copyright (c) 2022 Ashlynn Anderson
 #
 # Regards to Dmitry Mikheev for writing the original add-on this was
 # derived from, and the Anki team.
 
+from typing import Literal
+
+from anki.collection import CardIdsLimit
+import aqt.gui_hooks as gui_hooks
 from aqt.reviewer import Reviewer
+from anki.cards import Card
+
+from anki.utils import point_version
 from anki.hooks import wrap
 
-## CHANGE ANSWER BUTTONS TO PASS/FAIL ##
+# Hooks
+def pf2_replace_buttons(
+        buttons_tuple: tuple[tuple[int, str], ...],
+        reviewer: Reviewer,
+        card: Card
+) -> tuple[tuple[int, str], ...]:
+    return (
+        (1, "Fail"),
+        (2, "Pass")
+    )
 
-# Hardcoding the shortcut keys is a lazy solution,
-# but one I'm reasonably okay with
-def pfAnswerButtonList(self):
-    l = ((1, _("Fail"), _("Shortcut key: 1")),)
-    cnt = self.mw.col.sched.answerButtons(self.card)
-    if cnt == 2 or cnt == 3:
-        return l + ((2, _("Pass"), _("Shortcut keys: 2, 3, 4")),)
+def pf2_remap_answer_ease(
+        ease_tuple: tuple[bool, Literal[1, 2, 3, 4]],
+        reviewer: Reviewer,
+        card: Card
+) -> tuple[bool, Literal[1, 2, 3, 4]]:
+    (cont, ease) = ease_tuple
+    if ease == 1:
+        return ease_tuple
     else:
-        return l + ((3, _("Pass"), _("Shortcut keys: 2, 3, 4")),)
+        return (cont, reviewer._defaultEase())
 
-# For some reason it's necessary to copy over this function instead of
-# just wrapping _answerButtonList, I couldn't say why.
-def pfAnswerButtons(self, _old):
-    default = self._defaultEase()
-    def but(i, label, shortcutDialog):
-        cnt = self.mw.col.sched.answerButtons(self.card)
-        if i == default:
-            extra = "id=defease"
-        else:
-            extra = ""
-        due = self._buttonTime(i)
-        return '''
-<td align=center>%s<button %s title="%s" data-ease="%s" onclick='pycmd("ease%d");'>\
-%s</button></td>''' % (due, extra, shortcutDialog, i, i, label)
-    buf = "<center><table cellpading=0 cellspacing=0><tr>"
-    for ease, label, shortcutDialog in pfAnswerButtonList(self):
-        buf += but(ease, label, shortcutDialog)
-    buf += "</tr></table>"
-    script = """
-<script>$(function () { $("#defease").focus(); });</script>"""
-    return buf + script
+def pf2_force_defaultEase(_self) -> Literal[2, 3]:
+    return 2
 
-## MAP KEYBINDS TO REFLECT CHANGE ##
+# Shims for old versions of anki
+def pf2_shim_answerButtonList(self, _old) -> tuple[tuple[int, str], ...]:
+    result = _old(self)
+    return pf2_replace_buttons(result, self, self.card)
 
-# For 2/3 buttons, 1=FAIL/1, 2/3/4=PASS(2)
-# For 4 buttons, 1=FAIL/1, 2/3/4=PASS(3)
-remap = {
-    2:  [None, 1, 2, 2, 2],
-    3:  [None, 1, 2, 2, 2],
-    4:  [None, 1, 3, 3, 3]
-} 
+def pf2_shim_answerCard(self, ease: Literal[1, 2, 3, 4], _old) -> None:
+    (_, new_ease) = pf2_remap_answer_ease((True, ease), self, self.card)
+    return _old(self, new_ease)
 
-def pfAnswerCard(self, ease, _old):
-    cnt = self.mw.col.sched.answerButtons(self.card)
-    try:
-        ease = remap[cnt][ease]
-    except (KeyError, IndexError):
-        pass
-    _old(self, ease)
-
+# Init
 def init():
-    Reviewer._answerButtons =\
-        wrap(Reviewer._answerButtons, pfAnswerButtons, 'around')
-    Reviewer._answerCard =\
-        wrap(Reviewer._answerCard, pfAnswerCard, 'around')
+    version = point_version()
+
+    # Answer button list
+    if version >= 31:
+        gui_hooks.reviewer_will_init_answer_buttons.append(pf2_replace_buttons)
+    else:
+        Reviewer._answerButtonList = wrap(Reviewer._answerButtonList, pf2_shim_answerButtonList, 'around')
+
+    # Remap ease for keybinds
+    if version >= 20:
+        gui_hooks.reviewer_will_answer_card.append(pf2_remap_answer_ease)
+    else:
+        Reviewer._answerCard = wrap(Reviewer._answerCard, pf2_shim_answerCard, 'around')
+
+    # Fixes some default behavior, including keybind popup
+    Reviewer._defaultEase = pf2_force_defaultEase
